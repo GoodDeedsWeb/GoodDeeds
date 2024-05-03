685993599c063bea2664e0c1376e7ef44a80febd
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-03 10:06:54 +0300

    Configure database in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { AutoMapperModule } from './automapper/autoMapper.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './db_config/db.config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AutoMapperModule],
+  imports: [
+    UserModule, 
+    AutoMapperModule, 
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      load: [dbConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => (configService.get('db_config'))
+    }), 
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
