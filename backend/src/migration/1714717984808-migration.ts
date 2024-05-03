e67455791ad08197b533502152962d0e13852cf8
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-03 10:09:07 +0300

    Create migration

---
+++ backend/src/migration/1714717984808-migration.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migration1714717984808 implements MigrationInterface {
+  name = 'Migration1714717984808';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
