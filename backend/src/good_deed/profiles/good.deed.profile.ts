39b3d098c96599272a5cf66094118a483ca26c99
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-05 10:34:41 +0300

    Updated GoodDeedProfile

--- backend/src/good_deed/profiles/good.deed.profile.ts
+++ backend/src/good_deed/profiles/good.deed.profile.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Mapper, createMap, forMember, ignore } from '@automapper/core';
+import { Mapper, createMap } from '@automapper/core';
 import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
 import { Injectable } from '@nestjs/common';
 import { GoodDeed } from 'src/entities/db_entities/good.deed';
 import { GoodDeedCreateDto } from 'src/entities/good_deed_dto/good.deed.create.dto';
-import { GoodDeedDto } from 'src/entities/good_deed_dto/good.deed.dto';
+import { GoodDeedDeleteDto } from 'src/entities/good_deed_dto/good.deed.delete.dto';
 import { GoodDeedUpdateDto } from 'src/entities/good_deed_dto/good.geed.update.dto';
 
 @Injectable()
@@ -14,10 +14,10 @@ export class GoodDeedProfile extends AutomapperProfile {
   }
 
   override get profile() {
-    return (mapper) => {
-      createMap(mapper, GoodDeedCreateDto, GoodDeed, forMember((destination) => destination.Id, ignore()));
-      createMap(mapper, GoodDeedUpdateDto, GoodDeed, forMember((destination) => destination.Id, ignore()));
-      createMap(mapper, GoodDeed, GoodDeedDto)
+    return (mapper: Mapper) => {
+      createMap(mapper, GoodDeedCreateDto, GoodDeed);
+      createMap(mapper, GoodDeedDeleteDto, GoodDeed);
+      createMap(mapper, GoodDeedUpdateDto, GoodDeed)
     };
   }
 }
