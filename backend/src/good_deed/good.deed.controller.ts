127e65c9ce2a2f3bb968f7c5c61a5e7a51be567a
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-05 10:36:06 +0300

    Create GoodDeedController and GoodDeedModule

---
+++ backend/src/good_deed/good.deed.controller.ts
@@ -0,0 +1,64 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpStatus, Inject, Post, Put, Query, Request, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { IGoodDeedService } from 'src/interfaces/services/good.deed.service.interface';
+import { GoodDeedCreateDto } from 'src/entities/good_deed_dto/good.deed.create.dto';
+import { AuthenticationGuard } from 'src/user/guard/authentication.guard';
+import { GoodDeedUpdateDto } from 'src/entities/good_deed_dto/good.geed.update.dto';
+import { GoodDeedDeleteDto } from 'src/entities/good_deed_dto/good.deed.delete.dto';
+import { UserGoodDeedsDto } from 'src/entities/good_deed_dto/user.good.deeds.dto';
+
+@Controller('good-deed')
+export class GoodDeedController {
+  constructor(@Inject('IGoodDeedService') private readonly goodDeedService: IGoodDeedService) {}
+
+  @UseGuards(AuthenticationGuard)
+  @Post()
+  async createGoodDeed(@Body() goodDeedCreate: GoodDeedCreateDto, @Request() req, @Res({ passthrough: true }) res: Response): Promise<string> {
+    const result = await this.goodDeedService.createGoodDeed(goodDeedCreate, req.user['sub']);
+
+    res.status(result.statusCode);
+
+    return result.message;
+  }
+
+  @UseGuards(AuthenticationGuard)
+  @Get()
+  async getUserGoodDeeds(@Query('userId') userId: string, @Res({ passthrough: true }) res: Response): Promise<UserGoodDeedsDto> {
+    const numberUserId = Number(userId);
+
+    if (!numberUserId) {
+      res.status(HttpStatus.BAD_REQUEST);
+      return;
+    }
+
+    const userGoodDeeds = await this.goodDeedService.findByUserId(Number(userId));
+
+    if (!userGoodDeeds) {
+      res.status(HttpStatus.NOT_FOUND);
+      return;
+    }
+
+    return userGoodDeeds;
+  }
+
+  @UseGuards(AuthenticationGuard)
+  @Put()
+  async updateGoodDeed(@Body() goodDeedUpdate: GoodDeedUpdateDto, @Res({ passthrough: true }) res: Response){
+    const result = await this.goodDeedService.updateGoodDeed(goodDeedUpdate);
+
+    res.status(result.statusCode);
+
+    return result.message;
+  }
+
+  @UseGuards(AuthenticationGuard)
+  @Delete()
+  async deleteUser(@Body() goodDeedDelete: GoodDeedDeleteDto, @Res({ passthrough: true }) res: Response): Promise<string> {
+    const result = await this.goodDeedService.deleteGoodDeed(goodDeedDelete);
+
+    res.status(result.statusCode);
+
+    return result.message;
+  }
+}
---
+++ backend/src/good_deed/good.deed.module.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GOOD_DEED_REPOSITORY_TOKEN, GOOD_DEED_SERVICE_TOKEN } from 'src/constants/good.deed.tokens';
+import { GoodDeed } from 'src/entities/db_entities/good.deed';
+import { UserModule } from 'src/user/user.module';
+import { GoodDeedService } from './good.deed.service';
+import { GoodDeedController } from './good.deed.controller';
+import { GoodDeedRepository } from './good.deed.repository';
+import { GoodDeedProfile } from './profiles/good.deed.profile';
+
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([GoodDeed]),
+  ],
+  providers: [
+      GoodDeedProfile,
+      {
+        provide: GOOD_DEED_SERVICE_TOKEN,
+        useClass: GoodDeedService,
+      },
+      {
+        provide: GOOD_DEED_REPOSITORY_TOKEN,
+        useClass: GoodDeedRepository,
+      },
+    ],
+  controllers: [GoodDeedController],
+  exports: [GOOD_DEED_SERVICE_TOKEN, GOOD_DEED_REPOSITORY_TOKEN],
+})
+export class GoodDeedModule {}
