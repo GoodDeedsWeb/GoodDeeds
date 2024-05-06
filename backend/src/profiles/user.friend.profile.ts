d7b5650670be41f8048f9053a7a6cc82f6dc4339
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-06 14:36:13 +0300

    All profiles moved to separate folder

---
+++ backend/src/profiles/user.friend.profile.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Mapper, createMap } from '@automapper/core';
+import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
+import { Injectable } from '@nestjs/common';
+import { UserFriend } from 'src/entities/db_entities/user.friend';
+import { UserFriendCreateDto } from 'src/entities/user_friend_dto/user.friend.create.dto';
+import { UserFriendDeleteDto } from 'src/entities/user_friend_dto/user.friend.delete.dto';
+import { UserFriendSearchDto } from 'src/entities/user_friend_dto/user.friend.search.dto';
+
+@Injectable()
+export class UserFriendProfile extends AutomapperProfile {
+  constructor(@InjectMapper() mapper: Mapper) {
+    super(mapper);
+  }
+
+  override get profile() {
+    return (mapper) => {
+      createMap(mapper, UserFriendCreateDto, UserFriend);
+      createMap(mapper, UserFriendDeleteDto, UserFriend);
+      createMap(mapper, UserFriendSearchDto, UserFriend);
+    };
+  }
+}
