68b45e17e43be5c7d554583e415a6b50a5c075fd
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-09 19:05:39 +0300

    Edit token lifetime

--- backend/src/authentication_guard/authentication.guard.module.ts
+++ backend/src/authentication_guard/authentication.guard.module.ts
@@ -3,15 +3,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JWT_SECURITY_KEY } from '../constants/jwt.security.key';
 import { AuthenticationGuard } from '../authentication_guard/authentication.guard';
-import { AUTH_GUARD } from '../constants/guards';
-
+import { AUTH_GUARD } from '../constants/guard_tokens';
 
 @Module({
   imports: [
     JwtModule.register({
       global: false,
       secret: JWT_SECURITY_KEY,
-      signOptions: { expiresIn: '3600s' },
+      signOptions: { expiresIn: '7200s' },
     }),
   ],
   providers: [
