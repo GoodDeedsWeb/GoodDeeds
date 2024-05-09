3ada6caa5c472d3f8c138797a5c26501c9c2d59f
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-05-09 19:13:44 +0300

    Update cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    exposedHeaders: ['X-Pagination', 'X-IsFriend'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
